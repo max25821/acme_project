218e2fd02055eb26d4ad129a24fd1d705dc811b9
Max Dubnovickij <maksimdubnovickij22@gmail.com>
2026-07-25 12:17:44 +0300

    e

---
+++ acme_project/birthday/forms.py
@@ -0,0 +1,10 @@
+# birthday/forms.py
+from django import forms
+from .models import Birthday
+
+
+class BirthdayForm(forms.ModelForm):
+    class Meta:
+        model = Birthday
+        fields = "__all__"
+        widgets = {"birthday": forms.DateInput(attrs={"type": "date"})}
