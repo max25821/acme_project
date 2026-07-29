1a9880d8f41be4537a1538ea6669e3b4306ff31c
Max Dubnovickij <maksimdubnovickij22@gmail.com>
2026-07-29 12:02:13 +0300

    hello

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,22 +1,27 @@
 # birthday/models.py
 from django.db import models
+from django.urls import reverse
 
 # Импортируется функция-валидатор.
 from .validators import real_age
 
 
 class Birthday(models.Model):
-    first_name = models.CharField('Имя', max_length=20)
+    first_name = models.CharField("Имя", max_length=20)
     last_name = models.CharField(
-        'Фамилия', max_length=20, help_text='Опциональное поле', blank=True
+        "Фамилия", max_length=20, help_text="Опциональное поле", blank=True
     )
-    birthday = models.DateField('Дата рождения', validators=(real_age,))
-    image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
+    birthday = models.DateField("Дата рождения", validators=(real_age,))
+    image = models.ImageField("Фото", upload_to="birthdays_images", blank=True)
 
     class Meta:
         constraints = (
             models.UniqueConstraint(
-                fields=('first_name', 'last_name', 'birthday'),
-                name='Unique person constraint',
+                fields=("first_name", "last_name", "birthday"),
+                name="Unique person constraint",
             ),
         )
+
+    def get_absolute_url(self):
+        # С помощью функции reverse() возвращаем URL объекта.
+        return reverse("birthday:detail", kwargs={"pk": self.pk})
--- acme_project/pages/urls.py
+++ acme_project/pages/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 
-app_name = 'pages'
+app_name = "pages"
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
+    path("", views.HomePage.as_view(), name="homepage"),
 ]
